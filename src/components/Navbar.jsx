04cf471e40f57444af7dc3292a8012be668eccf9
Dev Shah <devkishorkumar1583@gmail.com>
2025-11-28 19:14:50 +0530

    First Push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "@constants/index";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Dev Shah</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
